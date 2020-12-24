08a3b108ab80dd54a50b3b43547c719ee647dc92
Atharva3001 <atharavtuljapurkar@gmail.com>
2020-12-25 00:42:00 +0530

    Changes in View product and view order

--- farmer_oh_farmer/src/View/view.js
+++ farmer_oh_farmer/src/View/view.js
@@ -5,6 +5,8 @@ import './loopimg.css';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import Order from '../Order/Order';
 
+
+
 class View extends Component{
   constructor(){
     super(); 
@@ -17,60 +19,72 @@ class View extends Component{
          src:'./images/tomatoes.jpg' ,
          price:"60Rs",
        },
-      //  {
-      //    rate:"250gm/",
-      //    src:'./images/tomatoes.jpg',
-      //    price:"60Rs"
-      //  },
-      //  {
-      //    rate:"250gm/",
-      //    src:'./images/tomatoes.jpg',
-      //    price:"60Rs"
+       {
+         rate:"250gm/",
+         src:'./images/tomatoes.jpg',
+         price:"60Rs"
+       },
+       {
+         rate:"250gm/",
+         src:'./images/tomatoes.jpg',
+         price:"60Rs"
 
-      //  },
-      //  {
-      //   rate:"250gm/",
-      //   src:'./images/tomatoes.jpg',
-      //   price:"60Rs"
-      //  },
-      //  {
-      //   rate:"250gm/",
-      //   src:'./images/tomatoes.jpg',
-      //   price:"60Rs"
-      //  },
-      //  {
-      //   rate:"250gm/",
-      //   src:'./images/tomatoes.jpg',
-      //   price:"60Rs"
-      //  },
+       },
+       {
+        rate:"250gm/",
+        src:'./images/tomatoes.jpg',
+        price:"60Rs"
+       },
+       {
+        rate:"250gm/",
+        src:'./images/tomatoes.jpg',
+        price:"60Rs"
+       },
+       {
+        rate:"250gm/",
+        src:'./images/tomatoes.jpg',
+        price:"60Rs"
+       },
         
      ],
   }
+
   render(){
    
+   
+    
     return(
-    
-    
-     <div className="Image">
       
-      <div className="block"> 
-     <div id="header">
+
+     
+    
+      <div className="Back"> 
+     <div className="Image">
+     
+      <div id="navbox">
+         <span id="Addpro">Addproduct</span> 
+         <Link to='/Order' className="Linked">Vieworder</Link>
+          <Link to='/Login' className="Linked2">Logout</Link>
+    <div id="header">
      <span id="header-text-1">Farmer</span>
       <span id="header-text-2">Oh</span>
       <span id="header-text-3">Farmer</span>
-     <div id="navbox">
-       <div className="flexBox">
-     <span id="Addpro">Addproduct</span> 
-        <Link to='/Order' className="Linked">Vieworder</Link>
+     
+       {/* <div className="flexBox"> */}
+     {/* <span id="Addpro">Addproduct</span>  */}
+        {/* <Link to='/Order' className="Linked">Vieworder</Link> */}
        {/* <img src={require("../Homepage/Login/logos/logout.png")} className="out" /> */}
-       <Link to='/Login' className="Linked2">Logout</Link>
+       {/* <Link to='/Login' className="Linked2">Logout</Link> */}
  
       </div>
        </div>
-      </div>
-      </div>   
+      
+      <div className="Block"> 
       <Loopimg imgData={this.state.img} />
-      </div>       
+      </div>
+      </div> 
+      </div> 
+      // </div>      
      
     );
 }
