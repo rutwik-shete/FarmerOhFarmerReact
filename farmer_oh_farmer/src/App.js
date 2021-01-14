a5d51b9f429671ea449fa0b8c5fd802bb63472c0
rutwik-shete <rutwik.shete@gmail.com>
2021-01-14 12:25:31 +0530

    Started Process To Add Menu BAr in Website Created Card For View Orders Page

--- farmer_oh_farmer/src/App.js
+++ farmer_oh_farmer/src/App.js
@@ -1,53 +1,32 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import View from "./View/view";
+import Order from "./Order/Order";
+import Login from "./Homepage/Login/Login";
+import MenuBar from "./MenuBar/MenuBar";
+// import Signup from "./SignUpPage/Signup";
 
-// import SignUp from './SignUpPage/Signup';
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import View from './View/view';
-import Order from './Order/Order';
-// <<<<<<< HEAD
-// =======
-import Login from './Homepage/Login/Login';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <MenuBar/>
+          <Switch>
+            <Route path="/" exact component={Login} />
+            <Route path="/view" component={View} />
+            <Route path="/Order" component={Order} />
+            <Route path="/Login" component={Login} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
 
-// import Signup from './SignUpPage/Signup';
-
-import Signup from './SignUpPage/Signup';
-import AddProduct from './Add product/AddProduct';
-
-
-// >>>>>>> 13495af22df56246065621b2649deb0c8e618beb
-class App extends Component{
-
-             
-  render(){
-return(
- 
-
-
-
-    <div>
-  
-  
-     
-   
-
-  
-    // <Router>
-    // <Switch>
-
-    //   <Route path="/" exact component={Login} />
-    //   <Route path="/view"  component={View} />
-    //   <Route path="/Order" component={Order} />
-    //   <Route path="/Login" component={Login} />
-    //   {/* <Route path="/Signup" component={SignUp} />  */}
-    
-    // </Switch>
-    // </Router>
-
-    </div>
-   
- 
-  );
-        }
-};
-
-export default App; 
+export default App;
